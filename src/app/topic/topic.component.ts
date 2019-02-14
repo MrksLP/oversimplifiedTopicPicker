8e6000c7ecbb65ba98a5a0f0c85139676155766a
markus <markus0endres@gmail.com>
2019-02-14 17:26:38 +0100

    Die Topics Liste wird nun kleiner dargestellt. Ein Wegräumen-Button wurde hinzugefügt um die Ausgabe wieder zu clearen

--- src/app/topic/topic.component.ts
+++ src/app/topic/topic.component.ts
@@ -10,6 +10,7 @@ import {forEach} from '@angular/router/src/utils/collection';
 export class TopicComponent implements OnInit {
 
   clickMessage: string;
+  allList: string;
 
   constructor() { }
 
@@ -17,44 +18,50 @@ export class TopicComponent implements OnInit {
   }
 
   onClickRandomTopic() {
-    const topics = environment.topicTechniques
-      .concat(environment.topicPlatforms)
-      .concat(environment.topicLanguagesFrameworks)
-      .concat(environment.topicTools);
+    this.clear();
+    const topics = this.getCombinedTopics();
     this.clickMessage = topics[this.getRandomInt(0, topics.length - 1)];
   }
-
   onClickAllTopic() {
-    const topics = environment.topicTechniques
-      .concat(environment.topicPlatforms)
-      .concat(environment.topicLanguagesFrameworks)
-      .concat(environment.topicTools);
-    this.clickMessage = this.getLinedArray(topics);
+    this.clear();
+    this.allList = this.getLinedArray(this.getCombinedTopics());
   }
 
   onClickRandomTools() {
+    this.clear();
     this.clickMessage = environment.topicTools[this.getRandomInt(0, environment.topicTools.length - 1)];
   }
   onClickAllTools() {
-    this.clickMessage = this.getLinedArray(environment.topicTools);
+    this.clear();
+    this.allList = this.getLinedArray(environment.topicTools);
   }
   onClickRandomLanguagesFrameworks() {
+    this.clear();
     this.clickMessage = environment.topicLanguagesFrameworks[this.getRandomInt(0, environment.topicLanguagesFrameworks.length - 1)];
   }
   onClickAllLanguagesFrameworks() {
-    this.clickMessage = this.getLinedArray(environment.topicLanguagesFrameworks);
+    this.clear();
+    this.allList = this.getLinedArray(environment.topicLanguagesFrameworks);
   }
   onClickRandomPlatforms() {
+    this.clear();
     this.clickMessage = environment.topicPlatforms[this.getRandomInt(0, environment.topicPlatforms.length - 1)];
   }
   onClickAllPlatforms() {
-    this.clickMessage = this.getLinedArray(environment.topicPlatforms);
+    this.clear();
+    this.allList = this.getLinedArray(environment.topicPlatforms);
   }
   onClickRandomTechniques() {
+    this.clear();
     this.clickMessage = environment.topicTechniques[this.getRandomInt(0, environment.topicTechniques.length - 1)];
   }
   onClickAllTechniques() {
-    this.clickMessage = this.getLinedArray(environment.topicTechniques);
+    this.clear();
+    this.allList = this.getLinedArray(environment.topicTechniques);
+  }
+
+  onClickClear() {
+    this.clear();
   }
 
   getRandomInt(min, max) {
@@ -67,5 +74,16 @@ export class TopicComponent implements OnInit {
       linedArray = s + '\r\n' + linedArray;
     }
     return linedArray;
+  }
+  private getCombinedTopics() {
+    const topics = environment.topicTechniques
+      .concat(environment.topicPlatforms)
+      .concat(environment.topicLanguagesFrameworks)
+      .concat(environment.topicTools);
+    return topics;
+  }
+  private clear() {
+    this.allList = '';
+    this.clickMessage = '';
   }
 }
