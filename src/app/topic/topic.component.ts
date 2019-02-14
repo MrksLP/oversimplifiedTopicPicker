a55da143d5112b79781ae655f2b2af7c587c1d1c
markus <markus0endres@gmail.com>
2019-02-14 07:55:31 +0100

    Diverse Anpassungen und Erweiterungen + Bugfix: Array-Index verwendung mit Array-Length

--- src/app/topic/topic.component.ts
+++ src/app/topic/topic.component.ts
@@ -1,9 +1,11 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {environment} from '../../environments/environment';
+import {forEach} from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-topic',
-  templateUrl: './topic.component.html'
+  templateUrl: './topic.component.html',
+  styleUrls: ['./topic.component.css']
 })
 export class TopicComponent implements OnInit {
 
@@ -14,27 +16,56 @@ export class TopicComponent implements OnInit {
   ngOnInit() {
   }
 
-  onClickTopic() {
+  onClickRandomTopic() {
     const topics = environment.topicTechniques
       .concat(environment.topicPlatforms)
       .concat(environment.topicLanguagesFrameworks)
       .concat(environment.topicTools);
-    this.clickMessage = topics[this.getRandomInt(1, topics.length)];
+    this.clickMessage = topics[this.getRandomInt(0, topics.length - 1)];
   }
-  onClickTools() {
-    this.clickMessage = environment.topicTools[this.getRandomInt(1, environment.topicTools.length)];
+
+  onClickAllTopic() {
+    const topics = environment.topicTechniques
+      .concat(environment.topicPlatforms)
+      .concat(environment.topicLanguagesFrameworks)
+      .concat(environment.topicTools);
+    this.clickMessage = this.getLinedArray(topics);
   }
-  onClickLanguagesFrameworks() {
-    this.clickMessage = environment.topicLanguagesFrameworks[this.getRandomInt(1, environment.topicLanguagesFrameworks.length)];
+
+  onClickRandomTools() {
+    this.clickMessage = environment.topicTools[this.getRandomInt(0, environment.topicTools.length - 1)];
   }
-  onClickPlatforms() {
-    this.clickMessage = environment.topicPlatforms[this.getRandomInt(1, environment.topicPlatforms.length)];
+  onClickAllTools() {
+    this.clickMessage = this.getLinedArray(environment.topicTools);
   }
-  onClickTechniques() {
-    this.clickMessage = environment.topicTechniques[this.getRandomInt(1, environment.topicTechniques.length)];
+  onClickRandomLanguagesFrameworks() {
+    this.clickMessage = environment.topicLanguagesFrameworks[this.getRandomInt(0, environment.topicLanguagesFrameworks.length - 1)];
+  }
+  onClickAllLanguagesFrameworks() {
+    this.clickMessage = this.getLinedArray(environment.topicLanguagesFrameworks);
+  }
+  onClickRandomPlatforms() {
+    this.clickMessage = environment.topicPlatforms[this.getRandomInt(0, environment.topicPlatforms.length - 1)];
+  }
+  onClickAllPlatforms() {
+    this.clickMessage = this.getLinedArray(environment.topicPlatforms);
+  }
+  onClickRandomTechniques() {
+    this.clickMessage = environment.topicTechniques[this.getRandomInt(0, environment.topicTechniques.length - 1)];
+  }
+  onClickAllTechniques() {
+    this.clickMessage = this.getLinedArray(environment.topicTechniques);
   }
 
   getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  private getLinedArray(stringArray) {
+    let linedArray = '';
+    for (const s of stringArray) {
+      linedArray = s + '\r\n' + linedArray;
+    }
+    return linedArray;
   }
 }
